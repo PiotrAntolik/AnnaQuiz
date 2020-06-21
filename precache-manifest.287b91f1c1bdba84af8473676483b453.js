83a9ece8e6ccdff42c9ef8fdb83b2f7531312b5c
Piotr Antolik <antolcrew@gmail.com>
2020-06-21 11:39:26 +0200

    Updates

---
+++ precache-manifest.287b91f1c1bdba84af8473676483b453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f180db3ccb0b08b2e3a4ddf9f60266b3",
+    "url": "/AnnaQuiz/index.html"
+  },
+  {
+    "revision": "02a57ce8cb53b5487b6e",
+    "url": "/AnnaQuiz/static/css/main.5e046027.chunk.css"
+  },
+  {
+    "revision": "911cb6ffb07f65162325",
+    "url": "/AnnaQuiz/static/js/2.526c1853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AnnaQuiz/static/js/2.526c1853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a57ce8cb53b5487b6e",
+    "url": "/AnnaQuiz/static/js/main.7d1db3f7.chunk.js"
+  },
+  {
+    "revision": "c0e6ce2575d8280227e9",
+    "url": "/AnnaQuiz/static/js/runtime-main.21f184af.js"
+  }
+]);
